c8de483d36c952f26e6f9ccdf5d6d4ba3047a450
李威德 <liweide@liweidedeMacBook-Air.local>
2023-11-04 10:57:45 +0800

    fix:刪除訂單及訂單細節新增邏輯,因為目前不需要

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -15,51 +15,6 @@ use App\Models\Order;
 class OrderController extends Controller
 {
     /**
-     * 訂單新增
-     * 
-     * @bodyParam total_price float required 訂單的總價格。
-     * @bodyParam payment_status string required 訂單的付款狀態，只能是 'paid', 'unpaid', 或 'canceled'。
-     * @bodyParam payment_method string required 訂單的付款方式，只能是 'credit_card' 或 'cash_on_delivery'。
-     * 
-     * @response 201 {
-     *     "id": "Newly created order ID"
-     * }
-     * 
-     * @response 400 {
-     *     "message": "The given data was invalid.",
-     *     "errors": {
-     *         "field_name": ["The field_name is required."]
-     *     }
-     * }
-     * 
-     * @response 401 {
-     *     "message": "Unauthenticated."
-     * }
-     * 
-     * @response 500 {
-     *     "message": "Server Error"
-     * }
-
-     * 
-     * @param \App\Http\Requests\OrderRequest $request 使用者輸入的請求數據。
-     * 
-     * @return \Illuminate\Http\Response 返回新創建的訂單ID的JSON響應或失敗的錯誤訊息。
-     */
-    public function store(OrderRequst $request)
-    {
-        $validatedData = $request->validated();
-        $userId = auth()->user()->id;
-        $order = Order::create([
-            'user_id' => $userId,
-            'total_price' => $validatedData['total_price'],
-            'payment_status' => $validatedData['payment_status'],   // 自动应用 setPaymentStatusAttribute
-            'payment_method' => $validatedData['payment_method'],   // 自动应用 setPaymentMethodAttribute
-            'status' => 'Pending'
-        ]);
-        return response()->json(['id' => $order->id], 201);
-    }
-
-    /**
      * 訂單列表
      * 獲取當前登錄用戶的所有訂單列表。
      * 
